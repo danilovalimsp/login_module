cb10e0a83e6ee592af0cac8481899f6d35c4225c
Danilo Valim <danilo.valim@guide.com.br>
2021-10-19 14:42:29 -0300

    added flow to home page

--- lib/app/login_page.dart
+++ lib/app/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:commons_app/app/dependencies/app_dependencies.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
@@ -8,15 +9,25 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  void openHomePage() {
+    Get.toNamed('home');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Login Page"),
-      ),
-      body: Center(
-        child: Text("Login Page"),
-      ),
-    );
+        appBar: AppBar(
+          title: Text("Login Page"),
+        ),
+        body: Container(
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Login Page"),
+              TextButton(onPressed: openHomePage, child: Text("Open Home Page"))
+            ],
+          ),
+        ));
   }
 }
